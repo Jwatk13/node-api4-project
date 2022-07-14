5ec4364396420824c2556e0b67a897fc75fbea7a
Joseph Watkins <josephxwatkins@gmail.com>
2022-07-14 14:43:51 -0400

    new endpoint

--- api/server.js
+++ api/server.js
@@ -5,10 +5,15 @@ const Users = require('./mock-db/db');
 
 server.use(express.json());
 
+
 server.get('/api/users', (req, res) => {
     Users.findAll()
     .then(users => res.json(users))
     .catch(err => res.status(500).json({ message: err.message }));
+});
+
+server.use('/', (req, res) => {
+    res.send(`<h1> Hello, world!</h1>`)
 });
 
 module.exports = server;
